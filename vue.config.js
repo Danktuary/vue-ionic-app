57bf055667ea09ac3024c448ff6f9abe7bc8ac0b
Sanctuary <sanc.pw@gmail.com>
2020-01-03 03:59:05 -0500

    Install/setup pwa plugin

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
 	lintOnSave: false,
+	pwa: {
+		themeColor: '#4DBA87',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+	},
 }
