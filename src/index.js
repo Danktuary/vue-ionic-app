10940523cc0e41ec94b95627f117aebd06396992
Sanctuary <sanc.pw@gmail.com>
2020-01-03 02:10:09 -0500

    `./router` -> `./router/index.js`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Ionic from '@ionic/vue'
 import App from './App.vue'
+import router from './router/index.js'
 import '@ionic/core/css/ionic.bundle.css'
 import './assets/css/ionic-variables.css'
 
